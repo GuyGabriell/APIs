184599f4047bd3bdd0da4347ab579f910373da9b
GuyGabriell <jonahgabriel85@yahoo.com>
2026-01-13 00:53:20 +0100

    first api call

---
+++ src/components/Form.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref } from "vue";
+
+const query = ref("");
+const gifs = ref([]);
+const fetchGifs = async () => {
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${
+      import.meta.env.VITE_GIPHY_API_KEY
+    }&q=${query.value}&limit=10&rating=g`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await response.json();
+  gifs.value = data.data; 
+
+};
+
+// fetchGifs();
+</script>
+
+<template>
+  <div class="flex flex-col items-center -center h-screen">
+    <!-- header-->
+    <h1 class="text-3xl font-bold text-blue-300 text-center mt-10 mb-5">
+      GIPHY MAKES SEARCH EASY...
+    </h1>
+    <!-- form/search-bar-->
+    <form
+      @submit.prevent="fetchGifs"
+      class="flex items-center border border-gray-300 rounded-md pl-2 mb-5"
+    >
+      <input
+        v-model="query"
+        type="text"
+        placeholder="Search for a GIF..."
+        class="outline-none"
+      />
+      <!-- search-button-->
+      <button
+        type="submit"
+        class="border rounded-md px-4 py-2 bg-blue-400 text-white"
+      >
+        Search
+      </button>
+    </form>
+    <!-- gifs-images-->
+    <div class="flex flex-wrap gap-6 items-center justify-center px-50">
+      <div v-for="gif in gifs" :key="gif.id">
+        <!-- Image -->
+        <img
+          :src="gif.images.original.url"
+          :alt="gif.title"
+          class="w-40 h-40 object-cover rounded-lg"
+        />
+
+        <!-- GIF Title -->
+        <h3 class="mt-2 font-bold text-gray-700 capitalize w-40 truncate">
+          {{ gif.title || "Untitled GIF" }}
+        </h3>
+
+        <!-- Creator Name -->
+        <p class="text-sm text-gray-500 italic">
+        {{ gif.user?.display_name || gif.username || "Anonymous" }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
